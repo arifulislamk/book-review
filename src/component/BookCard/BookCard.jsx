ddd1e85c77e56e87dbbbf51fa9b559eb570a2c33
arifulislamk <ariful18374@gmail.com>
2024-03-25 22:35:13 +0600

    book data load and card add

---
+++ src/component/BookCard/BookCard.jsx
@@ -0,0 +1,26 @@
+import { CiStar } from "react-icons/ci";
+const BookCard = ({ book }) => {
+
+    const { bookName, author, image, review,
+        totalPages, rating, category, tags, publisher, yearOfPublishing } = book;
+    return (
+        <div className=" p-6 card bg-base-100 border-2 border-gray-400 rounded-2xl shadow-xl">
+            <figure className="bg-[#F3F3F3] mb-7 p-10"><img src={image} alt="Shoes" /></figure>
+            <div className="card-body">
+                <div className="flex text-[#23BE0A] mb-5 gap-5">
+                    {tags.map(tag => <h3 key={tag}>{tag}</h3>)}
+                </div>
+                <h2 className=" mb-4 text-2xl font-bold text-[#131313]">
+                    {bookName}
+                </h2>
+                <p className=" mb-10 font-medium text-[16px] text-[#131313CC]">By: {author}</p>
+                <div className="flex justify-between text-[#131313CC] font-medium">
+                    <div className=" "><p>{category}</p></div>
+                    <div className=" flex items-center gap-3"> {rating} <CiStar className="text-xl"/></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BookCard;
--- src/component/Books/Books.jsx
+++ src/component/Books/Books.jsx
@@ -1,9 +1,22 @@
-const Books = () => {
+import { useEffect, useState } from "react";
+import BookCard from "../BookCard/BookCard";
 
+const Books = () => {
+    const [booksCard, setbooksCard] = useState([]);
+    useEffect(() => {
+        fetch('bookinfo.json')
+            .then(res => res.json())
+            .then(books => setbooksCard(books))
+    }, [])
     return (
         <div>
-            <h2>This books section</h2>
-            
+            <h2 className="mb-10 text-[40px] text-[#131313] font-bold text-center">Books : {booksCard.length}</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {
+                    booksCard.map( book => <BookCard key={book.id} book={book}></BookCard>)
+                }
+            </div>
+
         </div>
     );
 };
