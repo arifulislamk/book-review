1e48d00358f3b347d3fd78ccf1b0b6d7ff7c4da1
arifulislamk <ariful18374@gmail.com>
2024-03-26 17:33:11 +0600

    react icons add

--- src/component/ReadBook/ReadBook.jsx
+++ src/component/ReadBook/ReadBook.jsx
@@ -1,12 +1,28 @@
-const ReadBook = ({book}) => {
-    const {bookId, bookName, author, image, review,
-        totalPages, rating, category, tags, publisher, yearOfPublishing} = book ;
-    return (
-        <div>
-            <img src={image} alt="" />
-            <h2>{bookName}</h2>
+import { CiLocationOn } from "react-icons/ci";
 
-            
+const ReadBook = ({ book }) => {
+    const { bookId, bookName, author, image, review,
+        totalPages, rating, category, tags, publisher, yearOfPublishing } = book;
+    return (
+        <div className="grid grid-cols-4">
+            <div className="">
+                <img src={image} alt="" />
+            </div>
+            <div className=" col-span-3">
+                <h2>{bookName}</h2>
+                <p>By : {author}</p>
+                <div className="flex font-work font-medium mb-5 gap-5">
+                    <span>Tag</span> {tags.map((tag,inx) => <h3 className="flex text-[#23BE0A]" key={inx}>#{tag}</h3>)}
+                    <p> <CiLocationOn className=" inline mr-2 text-xl" /> Year of Publishing: {yearOfPublishing}</p>
+                </div>
+
+                <div className="flex gap-2">
+                    <p>Publisher: {publisher}</p>
+                    <p>Page {totalPages}</p>
+                </div>
+            </div>
+
+
         </div>
     );
 };
