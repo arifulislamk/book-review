320dcb67cee33a497330e53e3ad0f78761a2faf3
arifulislamk <ariful18374@gmail.com>
2024-03-26 20:20:57 +0600

    react icons

--- src/component/ReadBook/ReadBook.jsx
+++ src/component/ReadBook/ReadBook.jsx
@@ -1,9 +1,17 @@
 import { CiLocationOn } from "react-icons/ci";
 
 const ReadBook = ({ book }) => {
+
+
     const { bookId, bookName, author, image, review,
         totalPages, rating, category, tags, publisher, yearOfPublishing } = book;
+
+
+
+
+
     return (
+        
         <div className="grid grid-cols-4">
             <div className="">
                 <img src={image} alt="" />
@@ -12,8 +20,13 @@ const ReadBook = ({ book }) => {
                 <h2>{bookName}</h2>
                 <p>By : {author}</p>
                 <div className="flex font-work font-medium mb-5 gap-5">
-                    <span>Tag</span> {tags.map((tag,inx) => <h3 className="flex text-[#23BE0A]" key={inx}>#{tag}</h3>)}
-                    <p> <CiLocationOn className=" inline mr-2 text-xl" /> Year of Publishing: {yearOfPublishing}</p>
+                    <span>Tag</span> {tags.map((tag, inx) => <h3 className="flex text-[#23BE0A]" key={inx}>#{tag}</h3>)}
+                    <p className="flex "> <CiLocationOn className="mr-2 text-xl" /> Year of Publishing:  {yearOfPublishing}</p>
+                </div>
+                <div>
+                    <p>Category: {category}</p>
+                    <p>Rating: {rating}</p>
+                    <button>View Details </button>
                 </div>
 
                 <div className="flex gap-2">
